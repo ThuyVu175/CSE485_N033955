3afd8ec55ddd78e4ad3e5533207a69c80240d9f3
Nguyen Nga <ngantt62@wru.vn>
2018-12-29 20:43:46 +0700

    add

--- Sources/form_admin/menu_admin.php
+++ Sources/form_admin/menu_admin.php
@@ -1,49 +1,36 @@
+<nav class="navbar navbar-expand-lg sticky-top " style=" background:#838B8B;">
+		<div class="container">
+			<a class="navbar-brand" href="#"><i class="fa fa-lg fa-home" id="home"></i></a>
+			<button class="navbar-toggler navbar-toggler-right btn " type="button" data-toggle="collapse" data-target="#navb">
+				<span class=" ">Menu</span>
+			</button>			
+			<div class="collapse navbar-collapse" id="navb">
+				<ul class="navbar-nav mr-auto"  >
+                    <li class="nav-item">
+						<a class="nav-link" href="admin.php?page=account" ><font size="4" color="white">Quản Lí Tài Khoản</font></a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" href="admin.php?page=student" ><font size="4" color="white">Quản Lí Học Viên</font></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="admin.php?page=lecturer" ><font size="4" color="white">Quản Lí Giảng Viên</font></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="admin.php?page=category"><font size="4"  color="white">Quản Lí Danh Mục</font></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link " href="admin.php?page=subject"><font size="4" color="white">Quản Lí Môn Học</font></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link " href="admin.php?page=class"><font size="4" color="white">Quản Lí Lớp</font></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link " href="admin.php?page=sign_up"><font size="4" color="white">Quản Lí Đăng Kí</font></a>
+					</li>
+        </ul>
+			</div>
+		</div>
+	</nav>	
 
 
-<nav class="navbar navbar-expand-md navbar-dark sticky-top" id="menu">
-    <a class="navbar-brand" href="#">
-      <img src="../img/home.jpg" style="width: 40px; ">
-    </a>
-    <button class="navbar-toggler" data-toggle="collapse" data-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav ">
-        <li class="nav-item " >
-          <a class="nav-link" href="admin.php?page=lecturer" >
-            <font face="Verdana" size="3" color="white"> QUẢN LÍ GIẢNG VIÊN </font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="admin.php?page=student" >
-            <font face="Verdana" size="3" color="white">QUẢN LÍ HỌC VIÊN</font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="admin.php?page=category" >
-            <font face="Verdana" size="3" color="white">QUẢN LÍ DANH MỤC</font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="admin.php?page=subject" >
-            <font face="Verdana" size="3" color="white">QUẢN LÍ MÔN HỌC</font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="admin.php?page=class" >
-            <font face="Verdana" size="3" color="white">QUẢN LÍ LỚP</font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="admin.php?page=sign_up" >
-            <font face="Verdana" size="3" color="white">QUẢN LÍ ĐĂNG KÍ</font>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#tongquan">
-            <font face="Verdana" size="3" color="white">TỔNG QUAN</font>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+
--- Sources/form_admin/student/student.php
+++ Sources/form_admin/student/student.php
@@ -1,9 +1,3 @@
-
-  <script>
-    function xoahv() {
-      alert('Học viên đã được xóa!');
-    }
-  </script>
   <?php
    $servername = "localhost";
    $username = "root";
@@ -14,92 +8,57 @@
   if (!$connect) {
       die('kết nối không thành công ' . mysqli_connect_error());
   }
+  mysqli_set_charset($connect, 'UTF8');
+  $accountID = "";
+  $name = "";
+  $paid = "";
+  $owed = "";
+  $age = "";
+  $phoneNumber = "";
+  $email = "";
+  $sexual = "";
+
+  //Lấy giá trị POST từ form vừa submit
+  if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    if(isset($_POST["accountID"])) { $accountID = $_POST['accountID']; }
+    if(isset($_POST["name"])) { $name = $_POST['name']; }
+    if(isset($_POST["paid"])) { $paid = $_POST['paid']; }
+    if(isset($_POST["owed"])) { $owed = $_POST['owed']; }
+    if(isset($_POST["age"])) { $age = $_POST['age']; }
+    if(isset($_POST["phoneNumber"])) { $phoneNumber = $_POST['phoneNumber']; }
+    if(isset($_POST["email"])) { $email = $_POST['email']; }
+    if(isset($_POST["sexual"])) { $sexual = $_POST['sexual']; }
+    //Code xử lý, insert dữ liệu vào table
+    $sql = "INSERT INTO student (accountID,name,paid,owed,age,phoneNumber,email,sexual, createDate, isActive)
+    VALUES ('$accountID', '$name','$paid','$owed','$age','$phoneNumber','$email','$sexual', NOW(),1)";
+    if ( (mysqli_query($connect, $sql)) ) {
+      echo "<script>";
+      echo "alert('Thêm Học Viên Thành Công !');";  
+      echo "</script>";
+    } 
+    else {      
+    echo "<script>";
+    echo "alert('Thêm Học Viên Thất Bại !');";  
+    echo "</script>";
+  }
+}
+//Đóng database
+mysqli_close($connect);
   ?>
 <body style="background: #E6E6FA">
   <!--noi dung-->
   <div class="container">
     <div class="row">
       <div class="col-md-3 ">
-        <form title="THÊM GIẢNG VIÊN">
+        <form title="THÊM HỌC VIÊN" action="" method="post">
           <button type="button" class="button1 " data-toggle="modal" data-target="#addStudent">
             <font face="cursive"> ADD</font>
           </button>
- 
-        </form>
-      </div>
-      <div class="col-md-5 offset-md-4" style="padding-top: 50px;">
-        <button type="submit" class="buttonsearch">
-           <img src="../img/search.png" style="width: 34px;height: auto; ">
-        </button>
-        <input type="text" name="search" placeholder="Tìm kiếm ...">
-      </div>
-    </div>
-    <br><br>
-    <!---->
-    <div class="row">
-      <table class="table table-hover" bgcolor="">
-        <thead style="background: #CCCC66;">
-          <td>STT</td>
-          <td>Tên Học Viên</td>
-          <td>Tuổi</td>
-          <td>Số Điện Thoại</td>
-          <td>Email</td>
-          <td>Ngày Tạo</td>
-          <td>Chức Năng</td>
-        </thead>
-        <tbody>
-          <?php
-            // khởi tạo kết nối
-            $connect = mysqli_connect('localhost', 'root', '', 'qlkhoahocttnk');
-            // để hiện thị tiếng việt
-            mysqli_set_charset($connect, 'UTF8');
-            //Kiểm tra kết nối
-            if (!$connect) {
-                die('kết nối không thành công ' . mysqli_connect_error());
-            }
-
-            $sql = "SELECT * FROM student";
-            //kiểm tra
-            if ($result = mysqli_query($connect, $sql)) {
-                while ($row = mysqli_fetch_array($result)) {
-                  ?>
-          <tr>
-            <td><?php echo $row['ID'] ?></td>
-            <td><?php echo $row['name'] ?></td>
-            <td><?php echo $row['age'] ?></td>
-            <td><?php echo $row['phoneNumber'] ?></td>
-            <td><?php echo $row['email'] ?></td>
-            <td><?php echo $row['createDate'] ?></td>
-            <td>
-            <button type="button" title="SỬA THÔNG TIN HỌC VIÊN" class="buttonsmall" data-toggle="modal" data-target="#editStudent">
-                <h5> SỬA</h5>
-              </button>
-              
-              <button type="button" title="XÓA HỌC VIÊN" class="buttonsmall" onclick="xoahv();">
-                <h5>XÓA</h5>
-              </button>
-            </td>
-        </tr>
-        </tbody>
-          <?php
-              }
-          } else
-              //Hiện thông báo khi không thành công
-              echo 'Không thành công. Lỗi' . mysqli_error($connect);
-          //ngắt kết nối
-          mysqli_close($connect);
-          ?>
-      </table>
-    </div>
-  </div>
-</body>
-</html>
-
-<div class="modal fade" id="editStudent" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal fade" id="addStudent" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Sửa thông tin học viên</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Nhập thông tin học viên</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
@@ -108,34 +67,55 @@
                 <!--nội dung plugin-->
                 <table>
                   <tr>
-                    <td>Nhập tên Học Viên</td>
+                    <td>Nhập ID tài khoản Học Viên</td>
                     <td>
-                      <input type="text" name="Name.." id="ten_gv">
+                      <input type="text" name="accountID" >
                     </td>
                   </tr>
-              
+                  <tr>
+                    <td>Nhập tên Học Viên</td>
+                    <td>
+                      <input type="text" name="name" >
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Nhập học phí đã nộp</td>
+                    <td>
+                      <input type="text" name="paid" >
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Nhập học phí còn nợ</td>
+                    <td>
+                      <input type="text" name="owed" >
+                    </td>
+                  </tr>
                   <tr>
                     <td>Nhập tuổi</td>
                     <td>
-                      <input type="text" name="Date.." id="namsinh_gv">
+                      <input type="text" name="age" >
                     </td>
                   </tr>
                   <tr>
                     <td>Nhập số điện thoại</td>
                     <td>
-                      <input type="text" name="Date.." id="namsinh_gv">
+                      <input type="text" name="phoneNumber" >
                     </td>
                   </tr>
                   <tr>
                     <td>Nhập email</td>
                     <td>
-                      <input type="text" name="Date.." id="namsinh_gv">
+                      <input type="text" name="email" >
+                    </td>
+                  </tr>
+                  <td>Nhập giới tính</td>
+                    <td>
+                      <input type="text" name="sexual" >
                     </td>
                   </tr>
                 </table>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
               </div>
             </div>
@@ -190,3 +170,76 @@
               </div>
             </div>
           </div>
+        </form>
+      </div>
+      <div class="col-md-5 offset-md-4" style="padding-top: 50px;">
+        <button type="submit" class="buttonsearch">
+           <img src="../img/search.png" style="width: 34px;height: auto; ">
+        </button>
+        <input type="text" name="search" placeholder="Tìm kiếm ...">
+      </div>
+    </div>
+    <br><br>
+    <!---->
+    <div class="row">
+      <table class="table table-hover" bgcolor="">
+        <thead style="background: #CCCC66;">
+          <td hidden>ID</td>
+          <td>Tên Học Viên</td>
+          <td>Tuổi</td>
+          <td>Số Điện Thoại</td>
+          <td>Email</td>
+          <td>Ngày Tạo</td>
+          <td>Chức Năng</td>
+        </thead>
+        <tbody>
+          <?php
+            // khởi tạo kết nối
+            $connect = mysqli_connect('localhost', 'root', '', 'qlkhoahocttnk');
+            // để hiện thị tiếng việt
+            mysqli_set_charset($connect, 'UTF8');
+            //Kiểm tra kết nối
+            if (!$connect) {
+                die('kết nối không thành công ' . mysqli_connect_error());
+            }
+
+            $sql = "SELECT * FROM student";
+            //kiểm tra
+            if ($result = mysqli_query($connect, $sql)) {
+                while ($row = mysqli_fetch_array($result)) {
+                  ?>
+          <tr>
+            <td hidden><?php echo $row['ID'] ?></td>
+            <td><?php echo $row['name'] ?></td>
+            <td><?php echo $row['age'] ?></td>
+            <td><?php echo $row['phoneNumber'] ?></td>
+            <td><?php echo $row['email'] ?></td>
+            <td><?php echo $row['createDate'] ?></td>
+            <td>
+              <a href="student/edit.php?ID=<?php echo $row['ID']; ?>" >
+                <button type="button" title="SỬA THÔNG TIN HỌC VIÊN" class="buttonsmall" data-toggle="modal" data-target="#editStudent">
+                  <h5> SỬA</h5>
+                </button>
+              </a>
+              <a href="student/delete.php?ID=<?php echo $row['ID']; ?>" class="delete">
+                <button type="button" title="XÓA HỌC VIÊN" class="buttonsmall" onclick="xoahv();">
+                  <h5>XÓA</h5>
+                </button>
+              </a>
+            </td>
+        </tr>
+        </tbody>
+          <?php
+              }
+          } else
+              //Hiện thông báo khi không thành công
+              echo 'Không thành công. Lỗi' . mysqli_error($connect);
+          //ngắt kết nối
+          mysqli_close($connect);
+          ?>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
+
