308b5905a160901dd44b3f02abaf793e86931ef9
membern9 <namta@winds.vn>
2019-01-02 22:44:15 +0700

    sửa

---
+++ Sources/form/formphp/Menu/LienHe.php
@@ -0,0 +1,44 @@
+<div class="container p3" style="padding: 50px;">
+	<div class="row">
+		<h2 class="text-danger">TRUNG TÂM PHÁT TRIỂN NGHỆ THUẬT EAM</h2>
+	</div>
+	<div class="row font-weight-bold">
+		<P>THÔNG TIN TRUNG TÂM</P>
+	</div>
+	<div class="row font-weight-bold">	
+		<div class="col-md-2	">
+			<label>Địa chỉ:</label>
+		</div>
+		<div class="col-md-6">  
+			<label>Số 175, Tây Sơn, Đống Đa, Hà Nội</label>
+		</div>
+	</div>
+	<div class="row font-weight-bold">
+		<div class="col-md-2	">
+			<label>Điện thoại:</label>
+		</div>
+		<div class="col-md-6">  
+			<label class="text-danger">0965520566</label> <br>
+			<label class="text-danger">0965520566</label>
+		</div>
+	</div>
+	<div class="row font-weight-bold">
+		<div class="col-md-2">
+			<label>Email:</label>
+		</div>
+		<div class="col-md-6">  
+			 <label>eam@gmail.com</label>
+		</div>
+	</div>
+	<div class="row font-weight-bold">
+		<div class="col-md-2	">
+			<label>Website:</label>
+		</div>
+		<div class="col-md-6">  
+			<a href="#">trungtamnangkhieuEAM.com</a>
+		</div>
+	</div>
+	
+</div>
+
+
